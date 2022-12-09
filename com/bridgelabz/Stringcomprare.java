24bc113ad937ab7e7bfb0490836d7724dd0f0978
Ashuhauhan <ashuc306@gmail.com>
2022-12-09 19:00:35 +0530

    basic java programs

---
+++ com/bridgelabz/Stringcomprare.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+import java.util.*;
+public class Stringcomprare {
+
+
+
+        public static void main(String[] args) {
+
+            System.out.println("Enter any string");
+            Scanner sc = new Scanner(System.in);
+            String s1 = sc.nextLine();
+            System.out.println("Enter other string");
+            String s2 = sc.nextLine();
+            if ((s1 == s2)) {
+                System.out.println("the entered strings are equal ");
+            } else {
+                System.out.println("the entered strings are not equal");
+            }
+
+
+        } }
---
+++ com/bridgelabz/Username.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+public class Username {
+    public static void main(String[] args) {
+
+        System.out.println("Enter your name");
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        System.out.println("hello  " + s1 );
+    }}
