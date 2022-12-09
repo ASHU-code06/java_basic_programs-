19db1ecbdf2c911dc8e2bceeb2369703a3ffb680
Ashuhauhan <ashuc306@gmail.com>
2022-12-09 19:09:31 +0530

    modified code

--- com/bridgelabz/Gitsteps.java
+++ com/bridgelabz/Gitsteps.java
@@ -1,3 +1,3 @@
-first create a local repositary using git init in java
-then use git add .
-and rest steps are same as boot camp
+//first create a local repositary using git init in java
+//then use git add .
+//and rest steps are same as boot camp
--- com/bridgelabz/Leapyear.java
+++ com/bridgelabz/Leapyear.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
 public class Leapyear {
-
-}
+    public static void main(String[] args){
+    System.out.println("hello");
+}}
