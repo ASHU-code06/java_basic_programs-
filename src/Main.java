6a46680e8a99ffd8f3d7a0f96f4c94fcbc499598
ASHU CHAUHAN <117000067+ASHU-code06@users.noreply.github.com>
2022-12-16 00:31:37 +0530

    Delete Main.java

--- src/Main.java
+++
@@ -1,5 +0,0 @@
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//    }
-//}//double empCheck = Math.floor(Math.random() * 10) % 2;
